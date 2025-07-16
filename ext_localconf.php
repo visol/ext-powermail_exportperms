f1a21253591fb99632e5cd2b21f1c19d313f9204
Andy Meier <andy.meier@visol.ch>
2025-07-16 14:40:56 +0200

    task: remove RecordListGetTableHook

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,8 +1,4 @@
 <?php
-use Visol\PowermailExportperms\Hooks\RecordListGetTableHook;
 if (!defined('TYPO3')) {
-    die ('Access denied.');
+    die('Access denied.');
 }
-
-// Implement the getTable hook in the RecordList to manipulate the result of the query if the user doesn't have permission
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/class.db_list_extra.inc']['getTable']['powermail_exportperms'] = RecordListGetTableHook::class;
