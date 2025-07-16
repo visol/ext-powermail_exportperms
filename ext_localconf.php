a9376b9a0dc4a93524eacdfb8e7c191b346c887c
Andy Meier <andy.meier@visol.ch>
2025-07-16 14:44:47 +0200

    fix: set powermail template override by defaultPageTSconfig

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,4 +1,7 @@
 <?php
+
 if (!defined('TYPO3')) {
     die('Access denied.');
 }
+
+$GLOBALS['TYPO3_CONF_VARS']['BE']['defaultPageTSconfig'] .= chr(10) . 'templates.in2code/powermail.1752658658 = visol/powermail-exportperms:Resources/Private/TemplateOverrides';
